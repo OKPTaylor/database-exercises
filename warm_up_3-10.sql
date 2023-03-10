24dc9f8d9603e748c20d27b2a0f6a7f55ad72b04
OKPTaylor <o.k.p.taylor@gmail.com>
2023-03-10 09:34:18 -0600

    warm_up_3-10 added

---
+++ warm_up_3-10.sql
@@ -0,0 +1,10 @@
+SHOW databases;
+Use chipotle;
+select daTAbase();
+show tables;
+Select * From orders;
+SELECT * FROM orders
+WHERE quantity > 1 and 
+(item_name = "chicken bowl" or item_name = 'veggie bowl');
+
+
