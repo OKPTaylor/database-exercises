24048f5808fedf5e0e6934837ffbe3e744adbfe2
OKPTaylor <o.k.p.taylor@gmail.com>
2023-03-14 10:37:55 -0500

    add stuff to join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -14,6 +14,12 @@ left join users on roles.id = users.role_id;
 select * from roles
 right join users on roles.id = users.role_id;
 
+select roles.name as role_name,
+count(users.name) as number_of_employees
+from users
+right join roles on users.role_id = roles.id
+group by role_name;
+
 use employees;
 select database();
 show tables;
@@ -24,29 +30,27 @@ select * from salaries;
 select * from dept_emp;
 select * from titles;
 
-select departments.dept_name, concat(employees.first_name, " ",employees.last_name) as Department_Manager
+-- 2
+select departments.dept_name, concat(employees.first_name, " ",employees.last_name) as Department_Manager 
 from departments
 join dept_manager on departments.dept_no = dept_manager.dept_no
-join employees on dept_manager.emp_no = employees.emp_no
-where to_date = "9999-01-01"
-order by departments.dept_name -- 1
+join employees on dept_manager.emp_no = employees.emp_no -- forget you can use "as"
+where to_date = "9999-01-01" -- can also use .to_date > curdate()
+order by departments.dept_name 
 ;
 
+-- 3
 select departments.dept_name, concat(employees.first_name," ",employees.last_name) as Manager_Name 
 from departments
 join dept_manager on departments.dept_no = dept_manager.dept_no
 join employees on dept_manager.emp_no = employees.emp_no
 where to_date = "9999-01-01" and gender = 'F'
-order by departments.dept_name -- 2
+order by departments.dept_name 
 ;
 
-select departments.dept_name, concat(employees.first_name, employees.last_name)  
-from departments
-join dept_manager on departments.dept_no = dept_manager.dept_no
-join employees on dept_manager.emp_no = employees.emp_no
-where to_date = "9999-01-01" and dept_name = "Customer Service"
-; -- 3
 
+
+-- 4
 select title, count(*) as Count
 from titles
 join employees on titles.emp_no = employees.emp_no
@@ -55,9 +59,10 @@ join departments on dept_emp.dept_no = departments.dept_no
 where titles.to_date = "9999-01-01" and departments.dept_name = "Customer Service"
 group by title
 order by title
-; -- 4
+; 
 
-select departments.dept_name, salaries.salary, concat(employees.first_name, " " ,employees.last_name) as name
+-- 5
+select departments.dept_name, concat(employees.first_name, " " ,employees.last_name) as name,  salaries.salary
 from departments
 join dept_manager on departments.dept_no = dept_manager.dept_no
 join employees on dept_manager.emp_no = employees.emp_no
@@ -65,29 +70,30 @@ join salaries on employees.emp_no = salaries.emp_no
 where salaries.to_date = "9999-01-01" and dept_manager.to_date = "9999-01-01"
 group by departments.dept_name, salaries.salary, employees.first_name, employees.last_name
 order by departments.dept_name 
-; -- 5
+; 
 
-select dept_emp.dept_no, departments.dept_name, count(*) as num_employees
-from titles
-join employees on titles.emp_no = employees.emp_no
-join dept_emp on employees.emp_no = dept_emp.emp_no
+-- 6
+select dept_emp.dept_no, departments.dept_name, count(emp_no) as num_employees
+from dept_emp
 join departments on dept_emp.dept_no = departments.dept_no
 where dept_emp.to_date = "9999-01-01" 
 group by dept_emp.dept_no, departments.dept_name
 order by dept_emp.dept_no
-;
+; 
 
-select departments.dept_name, round(avg(salary),0) as average_salary
+-- 7
+select departments.dept_name, round(avg(salary),2) as average_salary
 from departments
-join dept_manager on departments.dept_no = dept_manager.dept_no
-join employees on dept_manager.emp_no = employees.emp_no
-join salaries on employees.emp_no = salaries.emp_no
-where salaries.to_date = "9999-01-01" and dept_manager.to_date = "9999-01-01"
+join dept_emp on departments.dept_no = dept_emp.dept_no
+join salaries on dept_emp.emp_no = salaries.emp_no
+where salaries.to_date = "9999-01-01" and dept_emp.to_date = "9999-01-01"
 group by departments.dept_name
 order by departments.dept_name desc
-; -- 7
+limit 1
+; 
 
-select employees.first_name, employees.last_name, salaries.salary
+-- 8
+select employees.first_name, employees.last_name
 from departments
 join dept_emp on departments.dept_no = dept_emp.dept_no
 join employees on dept_emp.emp_no = employees.emp_no
@@ -95,26 +101,28 @@ join salaries on employees.emp_no = salaries.emp_no
 where salaries.to_date = "9999-01-01" and dept_emp.to_date = "9999-01-01" and departments.dept_name = "Marketing"
 group by employees.first_name, employees.last_name, salaries.salary
 order by salary desc
-; -- 8
+limit 1
+; 
 
-select employees.first_name, employees.last_name, max(salary)
+-- 9
+select employees.first_name, employees.last_name, max(salary) as salary, departments.dept_name
 from departments
 join dept_manager on departments.dept_no = dept_manager.dept_no
 join employees on dept_manager.emp_no = employees.emp_no
 join salaries on employees.emp_no = salaries.emp_no
 where salaries.to_date = "9999-01-01" and dept_manager.to_date = "9999-01-01"
-group by employees.first_name, employees.last_name
+group by employees.first_name, employees.last_name, departments.dept_name
 order by max(salary) desc
-; -- 9
+limit 1
+; 
 
+-- 10 
 select departments.dept_name, round(avg(salary),0) as average_salary
 from departments
 join dept_emp on departments.dept_no = dept_emp.dept_no
-join employees on dept_emp.emp_no = employees.emp_no
-join dept_manager on departments.dept_no = dept_manager.dept_no
-join salaries on employees.emp_no = salaries.emp_no
+join salaries on dept_emp.emp_no = salaries.emp_no
 group by departments.dept_name
-order by average_salary desc -- 10
+order by average_salary desc 
 ;
 
 select concat(employees.first_name, ' ' ,employees.last_name) as employee_name, departments.dept_name
